5629316c060488ebd558bc5f4f9b6158e763331c
Andrew Munsell <andrew@wizardapps.net>
2017-05-26 22:19:01 -0700

    Add database utils for getting and deleting individual library DBs

--- src/db/index.js
+++ src/db/index.js
@@ -14,6 +14,55 @@ module.exports = class Database {
 
         logger.debug('Using %s as the root data folder.', dataFolder);
 
-        this.settings = new PouchDB(path.join(dataFolder, 'settings.db'));
+        // Global databases
+        this.settings = new this.PouchDB(path.join(dataFolder, 'settings.db'));
+        this.users = new this.PouchDB(path.join(dataFolder, 'users.db'));
+        this.libraries = new this.PouchDB(path.join(dataFolder, 'libraries.db'));
+
+        // Library databases
+        this.__library = {};
+    }
+
+    /**
+     * Get the library database with the given ID, or create a new library database for that
+     * ID if one does not exist yet.
+     */
+    async library(id) {
+        if (this.__library[id]) {
+            return this.__library[id];
+        }
+
+        try {
+            await this.libraries.get(id);
+        } catch(e) {
+            if (e.status === 404) {
+                return null;
+            } else {
+                throw e;
+            }
+        }
+
+        const library = new this.PouchDB(path.join(this.dataFolder, `library-${id}.db`));
+        this.__library[id] = library;
+
+        return library;
+    }
+
+    /**
+     * Delete the library with the specified ID.
+     */
+    async deleteLibrary(id) {
+        const library = await this.library(id);
+        if (!library) {
+            throw new Error('The requested library does not exist.');
+        }
+
+        delete this.__library[id];
+        await library.destroy();
+
+        const libraryDoc = await this.libraries.get(id);
+        await this.libraries.remove(libraryDoc);
+
+        return true;
     }
 }
